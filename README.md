b31c3dece43ce9696763fd3a93855580f4e066be
Thomas Kjær <thomas.kjaer@teliacompany.com>
2018-01-21 22:20:29 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+SafariCookieJar.get_cookies() returns a nested dict:
+```
+{ 'url': { 'path': { 'name': { 'expiry_time': '', 'flags': '', 'last_access_time': '', 'value': '', } } } }
+```
+
+```sh
+$ python3 -c "import SafariCookieJar, pprint; pprint.pprint(SafariCookieJar.get_cookies())"
+{'lobste.rs': {'/': {'lobster_trap': {'expiry_time': 1519203368.0,
+                                      'flags': 5,
+                                      'last_access_time': 1516524969.0,
+                                      'value': 'Nis1TU9vK0FTTWtsVzI1anIwNGY2elFvNjB1ZEEzdktHaXhucVA5ZkxxM2szL2dXbU9OcEJ6empmQkNRMnk1dFJERTFYa09oTDVQdXFhVThMTW9jY0lrcGhpbnN2YUJIdmtPMWl5U3E2RVRtM2lsM1dJSlZjSVBkekF2bDFJSnZSMHhUTWN4emZpeElPZWE1Wkg1dGpnPT0tLW1tdE0yTHFIV1V4azUxU2p4VVBhR2c9PQ%3D%3D--8c532a7d1bf000c204907701e894f606f6fb0b47'}}}}
+```
---
+++ SafariCookieJar.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Safari Cookies.binarycookies parser.
+# 
+# Reads the binary cookies file and returns a nested dict.
+# 
+# 
+# Built based on the work by:
+# 
+# Satishb3 (http://www.securitylearn.net):
+# - http://securitylearn.net/wp-content/uploads/tools/iOS/BinaryCookieReader.py
+#
+# Locutus (https://it.toolbox.com/users/content/Locutus)
+# - https://it.toolbox.com/blogs/locutus/understanding-the-safari-cookiesbinarycookies-file-format-010712
+
+from struct import unpack
+from io import BytesIO
+from pathlib import Path
+from datetime import datetime
+
+binarycookies = str(Path.home()) + "/Library/Cookies/Cookies.binarycookies"
+
+def get_cookies():
+    CookieJar = {}
+
+    with open(binarycookies, "rb") as cookies_file:
+        # Field 1: 4 byte magic number = 'cook'
+        file_header = cookies_file.read(4)
+        if file_header != b'cook':
+            print("Not a Cookies.binarycookies file.")
+
+        # Field 2: 4 byte int = number of pages
+        num_pages = unpack('>i', cookies_file.read(4))[0]
+        # Field 3: 4 byte int (one for each page) = page length
+        page_sizes = [unpack('>i', cookies_file.read(4))[0] for n in range(num_pages)]
+
+        for page in [cookies_file.read(ps) for ps in page_sizes]:
+            # Convert the string to a file with
+            page = BytesIO(page)
+
+            # Field 1: 4 byte header: '\x00\x00\x01\x00'
+            page.read(4)
+
+            # Field 2: 4 byte int: number of cookies
+            num_cookies = unpack('<i', page.read(4))[0]
+
+            # Field 3: 4 byte int (one for each cookie) = cookie offset
+            cookie_offsets = [unpack('<i', page.read(4))[0] for n in range(num_cookies)]
+
+            # Field 4: 4 byte footer: '\x00\x00\x00\x00'
+            page_footer = unpack('>i', page.read(4))[0]
+
+            for offset in cookie_offsets:
+                cookie = {}
+
+                # seek to the cookie position in the page
+                page.seek(offset)
+
+                # Field 1: 4 byte int: cookie size
+                # get the cookie length and then the binary cookie content
+                cookie_bytes = BytesIO(page.read(unpack('<i', page.read(4))[0]))
+                # Field 2: 4 byte: '\x00\x00\x00\x00'
+                cookie_bytes.read(4)
+
+                # Field 3: 4 byte: cookie flags
+                cookie['flags'] = unpack('<i', cookie_bytes.read(4))[0]
+                # Field 4: 4 byte: '\x00\x00\x00\x00'
+                cookie_bytes.read(4)
+
+                # Field 5: 4 byte int: url field offset from cookie start
+                # Field 6: 4 byte int: name field offset from cookie start
+                # Field 7: 4 byte int: path field offset from cookie start
+                # Field 8: 4 byte int: value field offset from cookie start
+                offset_values = ['url','name','path','value']
+                content_offsets = dict(zip(offset_values,
+                    [unpack('<i', cookie_bytes.read(4))[0] for n in offset_values]
+                    ))
+
+                # Field 9: 8 byte footer: '\x00\x00\x00\x00\x00\x00\x00\x00'
+                cookie_offset_footer = cookie_bytes.read(8)
+
+                # Seconds between Mac Epoch and Unix Epoch
+                mac_epoch = int(datetime(2001,1,1).strftime("%s"))
+
+                # Field 10: 8 byte double: expiry time of cookie
+                # Field 11: 8 byte double: last access time of cookie
+                # time is in Mac Epoch - we change to Unix Epoch
+                cookie['expiry_time'] = unpack('<d', cookie_bytes.read(8))[0]+mac_epoch
+                cookie['last_access_time'] = unpack('<d', cookie_bytes.read(8))[0]+mac_epoch
+
+                # Field 12: variable length, null-terminated: cookie name
+                # Field 13: variable length, null-terminated: cookie value
+                # Field 14: variable length, null-terminated: cookie url
+                # Field 15: variable length, null-terminated: cookie path
+                for k in content_offsets.keys():
+                    # seek to the offset (-4 because .. ?) and read until we
+                    # hit the null-termination
+                    cookie_bytes.seek(content_offsets[k]-4)
+                    _byte = cookie_bytes.read(1)
+                    _value = ''
+                    while unpack('<b', _byte)[0] != 0:
+                        _value = _value + _byte.decode('ascii')
+                        _byte = cookie_bytes.read(1)
+                    cookie[k] = _value
+
+                # put the cookie in the jar
+                url = cookie.pop('url')
+                path = cookie.pop('path')
+                name = cookie.pop('name')
+                if url in CookieJar:
+                    if path in CookieJar[url]:
+                        CookieJar[url][path][name] = cookie
+                    else:
+                        CookieJar[url][path] = { name: cookie }
+                else:
+                    CookieJar[url] = { path: { name: cookie} }
+
+    return(CookieJar)
